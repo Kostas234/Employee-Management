91d99e8a6359aedb5f153e3bb9826eb1b3aef1e5
Kostas234 <zakkask77@gmail.com>
2022-01-23 21:48:26 +0100

    First commit

---
+++ EmployeeManagement.Api/Models/AppDbContext.cs
@@ -0,0 +1,93 @@
+﻿using EmployeeManagement.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Api.Models
+{
+    public class AppDbContext:DbContext
+    {
+        // We need an instance of the DbContextOPtions class so we create
+        // a constructor with a DbContextOptions instance as an argument named
+        // options and we pass this to the the base DBContext class and we do
+        // this using the base keyword
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+        {
+        }
+
+        // We need a DBSet project for each entity in the model so we create
+        // one for Employee and one for Department and we will use these to 
+        // query and save instances of Employee and Department classes
+        // The LINQ queries against the DbSet<TEntity> will be translated into queres against
+        // the underlying database
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Department> Departments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //Seed Departments Table
+            modelBuilder.Entity<Department>().HasData(
+                new Department { DepartmentId = 1, DepartmentName = "IT" });
+            modelBuilder.Entity<Department>().HasData(
+                new Department { DepartmentId = 2, DepartmentName = "HR" });
+            modelBuilder.Entity<Department>().HasData(
+                new Department { DepartmentId = 3, DepartmentName = "Payroll" });
+            modelBuilder.Entity<Department>().HasData(
+                new Department { DepartmentId = 4, DepartmentName = "Admin" });
+
+            // Seed Employee Table
+            modelBuilder.Entity<Employee>().HasData(new Employee
+            {
+                EmployeeId = 1,
+                FirstName = "John",
+                LastName = "Hastings",
+                Email = "David@pragimtech.com",
+                DateOfBrith = new DateTime(1980, 10, 5),
+                Gender = Gender.Male,
+                DepartmentId = 1,
+                PhotoPath = "images/john.png"
+            });
+
+            modelBuilder.Entity<Employee>().HasData(new Employee
+            {
+                EmployeeId = 2,
+                FirstName = "Sam",
+                LastName = "Galloway",
+                Email = "Sam@pragimtech.com",
+                DateOfBrith = new DateTime(1981, 12, 22),
+                Gender = Gender.Male,
+                DepartmentId = 2,
+                PhotoPath = "images/sam.jpg"
+            });
+
+            modelBuilder.Entity<Employee>().HasData(new Employee
+            {
+                EmployeeId = 3,
+                FirstName = "Mary",
+                LastName = "Smith",
+                Email = "mary@pragimtech.com",
+                DateOfBrith = new DateTime(1979, 11, 11),
+                Gender = Gender.Female,
+                DepartmentId = 1,
+                PhotoPath = "images/mary.png"
+            });
+
+            modelBuilder.Entity<Employee>().HasData(new Employee
+            {
+                EmployeeId = 4,
+                FirstName = "Sara",
+                LastName = "Longway",
+                Email = "sara@pragimtech.com",
+                DateOfBrith = new DateTime(1982, 9, 23),
+                Gender = Gender.Female,
+                DepartmentId = 3,
+                PhotoPath = "images/sara.png"
+            });
+        }
+    }
+}
